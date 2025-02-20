b8a9b910f1be21031722b060a3371ba0df15e3fa
Mohammad Hujefa <ghanchihujefa999@gmail.com>
2025-02-20 13:52:40 +0530

    login&signup done

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -28,23 +28,23 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  try {
-    if (!this.isModified('password')) {
-      return next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
+// userSchema.pre('save', async function (next) {
+//   try {
+//     if (!this.isModified('password')) {
+//       return next();
+//     }
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 // Compare entered password with stored password
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return bcrypt.compare(enteredPassword, this.password);
-};
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//   return bcrypt.compare(enteredPassword, this.password);
+// };
 
 const User = mongoose.model('User', userSchema);
 
